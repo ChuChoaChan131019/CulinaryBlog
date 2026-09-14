5f41e1f4c8d5bbcdb1faa5ea0dab85077b61fa58
dopaemon <polarisdp@gmail.com>
2026-09-14 22:05:16 +0700

    feat(auth): thêm api đăng ký post /auth/register
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './infrastructure/database/database.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
